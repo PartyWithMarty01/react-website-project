1e2cff62751895df248d234ad0caa432503798d9
Martin C. Earle <82573922+PartyWithMarty01@users.noreply.github.com>
2025-03-09 08:11:21 +0100

    Updated code for the CRUD. - Update section - Added button.

--- src/GenStudentInfo.js
+++ src/GenStudentInfo.js
@@ -19,6 +19,38 @@ function GenStudentInfo() {
 
     console.log({studentData})
 
+    const handlePut = async (studentId) =>{
+        const confirmUpdate = window.confirm('Are you sure you want to update this student?')
+
+        if (!confirmUpdate) return;
+
+        try {
+            const response = await fetch(`http://localhost:4000/${studentId}`, {
+                method: "PUT",headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: "Updated Name",
+                    age: "Updated Age"
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to update student!")
+            }
+
+            alert(`Student ID ${studentId} updated successfully!`)
+
+            setStudentData(prevData => {
+                const updatedUsers = {...prevData.users};
+                updatedUsers[studentId] = { ...updatedUsers[studentId], name: "Updated Name", age: "Updated Age"};
+                return {users: updatedUsers};
+            });
+        }  catch (error) {
+            console.error("Error updating student:", error);
+            alert("Failed to update student");
+        }
+    };
     const handleDelete = async (studentId) => {
         const confirmDeletion = window.confirm('Are you sure you want to delete this student? (OK/CANCEL)')
 
@@ -65,6 +97,7 @@ function GenStudentInfo() {
                             <p>Name: {value.name}</p>
                             <p>Age: {value.age}</p>
                             <button onClick={() => handleDelete(key)}>Delete</button>
+                            <button onClick={() => handlePut(key)}>Update</button>
                         </div>
 })}
                 </div>
