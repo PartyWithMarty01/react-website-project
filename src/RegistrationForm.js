90280f5de50b3bfbacb76a63f274e51d96dcfdda
Emerick Bosch <xpc.dev@gmail.com>
2025-04-05 17:01:58 +0200

    Fix creating a new user using the registration form
    We were calling against the wrong endpoint
    (localhost:4000/registration-from instead of localhost:4000/) and using
    the wrong method (POST instead of PUT) AND we needed to prevent the
    default behaviour of the browser when submitting the form.

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const RegistrationForm = () => {
   //Define state on the react component that will hold data of the form.
@@ -6,101 +6,78 @@ const RegistrationForm = () => {
     firstName: "",
     lastName: "",
     age: "",
-    email: "",
-    password: ""
   });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = (event) => {
-    //event.preventDefault();
+    event.preventDefault();
     console.log("Submitted Data:", formData);
 
     const studentInfo = {
       name: `${formData.firstName} ${formData.lastName}`,
-      email: formData.email,
-      password: formData.password,
-      age: formData.age
+      age: formData.age,
     };
-  
-    const response = fetch("http://localhost:4000/registration-form", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json"
-     },
-     body:JSON.stringify(studentInfo)});
-  
+
+    const response = fetch("http://localhost:4000/", {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(studentInfo),
+    });
   };
 
   return (
     <div>
       <h2>Student Registration Form</h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Last Name:
-          <input
-            type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Age:
-          <input
-            type="age"
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Email Address:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <br />
+        <div>
+          <label>
+            First Name:
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Age:
+            <input
+              type="age"
+              name="age"
+              value={formData.age}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
         <button type="submit">Register</button>
       </form>
     </div>
   );
-
-}
+};
 export default RegistrationForm;
+
