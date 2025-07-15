67b26884e1abba83b7ae08bcf8ff1dd2325c4da9
Martin C. Earle <82573922+PartyWithMarty01@users.noreply.github.com>
2025-07-15 22:35:26 +0200

    Created read lessons on Lesson Manager
    I decided to read the students with lessons in the lessons manager and to toggle a display of the list w/ a button.
    Was fixing bugs most of the night but it works now.

--- src/LessonsManager.js
+++ src/LessonsManager.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 function LessonsManager() {
     const[selectedStudent, setSelectedStudent] = useState('');
     const [students, setStudents] = useState({});
+    const [showLessons, setShowLessons] = useState(false);
 
     useEffect(() => {
         fetchStudents();
@@ -20,6 +21,10 @@ function LessonsManager() {
     };
 
     const addLessons = async() => {
+        if (!selectedStudent){
+            alert("Please select a student first!");
+            return;
+        }
         //Here I will be adding a lesson to a student
         try {
         const res = await fetch('http://localhost:4000/lessons', {
@@ -31,14 +36,18 @@ function LessonsManager() {
 
         if (!res.ok) {
             throw new Error("Failed to create lesson!");
-      } }
-            catch(err) {
-                console.error(err);
-                alert("ERROR, your lesson was not created!")
+        } 
+            alert("Lesson created!");
+            fetchStudents();
+
+        } catch(err) {
+            console.error(err);
+            alert("ERROR, your lesson was not created!")
       }
     }
 
     return(
+       <div> 
         <div className="App">
             <h2>Lesson's Manager</h2>
             
@@ -52,8 +61,34 @@ function LessonsManager() {
                 ))}
             </select>
             <button onClick={addLessons}>Create Lesson</button>
+        
         </div>
-    )
-}
+        <hr />
+        <button onClick={() => setShowLessons((prev) => !prev)}>{showLessons ? "Hide Students & Lessons" : "Show Students & Lessons"}</button>
+        {showLessons && (
+        <div>
+            <h3>View students with registered lessons:</h3>
+            {Object.entries(students).map(([id, student]) => (
+                <div key={id} style={{marginBottom: "1rem"}}>
+                    <strong>
+                        {student.name}(Age: {student.age})
+                    </strong>
+                    {student.lessons.length > 0 ? (
+                        <ul>
+                            {student.lessons.map((lesson, index) => (
+                                <li key={index}>
+                                    Lesson ID: {lesson.lesson_id}, Created At: {lesson.created_at}
+                                </li>
+                            ))}
+                        </ul>
+                    ) : ( 
+                    <p>No Lessons created!</p>
+                    )}
+                </div>
+            ))}           
+        </div>
+        )}
+        </div>
+    );}
 
 export default LessonsManager;
